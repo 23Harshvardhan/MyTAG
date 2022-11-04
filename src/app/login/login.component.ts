6e815ea9ae3d01caa8e263db54fc01e88b1fc096
Harshvardhan <23harshvardhan@gmail.com>
2022-11-04 17:35:05 +0530

    Login Page Functional
    Login page is not fully functional.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router:Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  login(loginCreds : {Email:string, Password:string}){
+    axios.post('104.197.104.222/v1/api/auth/signin', {
+      data: loginCreds
+    })
+    .then ((response) => {
+      this.router.navigate(['/userDashboard']);
+    })
+    .catch ((error) => {
+      console.log(error);
+    })
+  }
 }
