0b8de9942c085b6a8f15d5f65f42edb5adfafe0c
Harshvardhan <23harshvardhan@gmail.com>
2022-11-04 23:02:58 +0530

    Cookies Enabled
    Cookie system enabled.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import axios from 'axios';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-signup',
@@ -9,13 +10,28 @@ import axios from 'axios';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  private cookie_name='';
+  private all_cookies:any='';
 
   constructor(
-    private http:HttpClient,
-    private router:Router
+    private router:Router,
+    private cookieService:CookieService
     ) {  }
 
+    setCookie(token:string){
+      this.cookieService.set('jwt', token);
+    }
+     
+    deleteCookie(cookieName:string){
+      this.cookieService.delete(cookieName);
+    }
+     
+    deleteAll(){
+      this.cookieService.deleteAll();
+    }
+
   ngOnInit(): void {
+    this.all_cookies=this.cookieService.getAll();
   }
 
   onSignUp (signUpCreds: {FirstName:string, LastName: string, Email:string, Password: string}){
@@ -25,6 +41,7 @@ export class SignupComponent implements OnInit {
     })
     .then( (response) => {
       alert("If the entered email is correct you will receive OTP");
+      this.setCookie(response.data.data.token);
     })
     .catch( (error) => {
       console.log(error);
@@ -32,18 +49,22 @@ export class SignupComponent implements OnInit {
   }
 
   checkOtp(otp: {otp:number}){
+    this.all_cookies=this.cookieService.getAll();
+    if(otp != null) {
       axios.post('http://104.197.104.222/v1/api/auth/verifyEmail', {
         data: otp
         })
         .then( (response) => {
-          alert("working");
-          console.log(response);
-          // this.router.navigate(['/userDashboard'])
+          this.router.navigate(['/userDashboard'])
         })
         .catch((error) => {
           console.log(error);
           alert("Something went wrong, please try again later.")
         });
+      } 
+      else {
+        alert("Invalid OTP");
+      }
     } 
 
   resendOtp() {
