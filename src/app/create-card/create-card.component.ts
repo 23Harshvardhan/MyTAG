d99ee36b668dc47ead438c86b87f2644bc7d87bd
Harshvardhan <23harshvardhan@gmail.com>
2022-11-08 23:59:41 +0530

    Create Card Page Fully Working
    Card Card page renders and takes input properly.
    Fixed small bugs.

--- src/app/create-card/create-card.component.ts
+++ src/app/create-card/create-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-create-card',
@@ -7,7 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private cookieService:CookieService,
+    private router:Router
+  ) { }
+
+  setCookie(token:string){
+    this.cookieService.set('jwt', token);
+  }
+   
+  deleteCookie(cookieName:string){
+    this.cookieService.delete(cookieName);
+  }
+   
+  deleteAll(){
+    this.cookieService.deleteAll();
+  }
 
   ngOnInit(): void {
   }
@@ -15,32 +33,85 @@ export class CreateCardComponent implements OnInit {
   currentBlockName = "";
   accreds:string[] = [];
 
+  socials:string[] = ["twitter","instagram","linkedin","facebook","snapchat","tiktok","twitch","yelp","youtube"];
+
   data = {
     name: "",
     designation: "",
     companyName: "",
-    accreditations: ""
+    accreditations: "",
+    email: "",
+    phone: "",
+    companyUrl: "",
+    link: "",
+    address: "",
+    twitter: "",
+    instagram: "",
+    linkedin: "",
+    facebook: "",
+    snapchat: "",
+    tiktok: "",
+    twitch: "",
+    yelp: "",
+    youtube: ""
+  }
+
+  postData = {
+    "Name": this.data.name,
+    "Job_title": this.data.designation,
+    "Department": this.data.designation,
+    "Company_name": this.data.companyName,
+    "Accreditations": this.data.accreditations,
+    "Headline": "",
+    "Email": this.data.email,
+    "Phone": this.data.phone,
+    "Company_URL": this.data.companyUrl,
+    "Link": this.data.link,
+    "Address": this.data.address,
+    "Twitter": this.data.twitter,
+    "Twitter_label": "",
+    "Instagram": this.data.instagram,
+    "Instagram_label": "",
+    "Linkedin": this.data.linkedin,
+    "Linkedin_label": "",
+    "Facebook": this.data.facebook,
+    "Facebook_label": "",
+    "Youtube": this.data.youtube,
+    "Youtube_label": "",
+    "Snapchat": this.data.snapchat,
+    "Snapchat_label": "",
+    "Tiktok": this.data.tiktok,
+    "Tiktok_label": "",
+    "Twitch": this.data.twitch,
+    "Twitch_label": "",
+    "Yelp": this.data.yelp,
+    "Yelp_label": ""
   }
 
   showInputWind(blockName:string) {
     this.currentBlockName = blockName;
+
+    const warning1 = document.getElementById("warning1");
+    const warning2 = document.getElementById("warning2");
+    
     var inputWind = document.getElementById("inputWind");
-    const warning = document.getElementById("warning");
     const inputArea = document.getElementById("textArea") as HTMLInputElement;
     inputWind!.style.display = "block";
     var temp = this.data[blockName as keyof typeof this.data];
     inputArea.value = temp;
 
     if(this.currentBlockName == "accreditations") {
-      warning!.style.display = "block";
+      warning1!.style.display = "block";
     }
     else{
-      warning!.style.display = "none";
+      warning1!.style.display = "none";
+      warning2!.style.display = "none";
     }
   }
 
   closeWind() {
     var inputWind = document.getElementById("inputWind");
+    var contactSection = document.getElementById("contactSection")
     const inputArea = document.getElementById("textArea") as HTMLInputElement;
     var temp = inputArea.value;
     this.data[this.currentBlockName as keyof typeof this.data] = temp;
@@ -53,38 +124,24 @@ export class CreateCardComponent implements OnInit {
     }
   }
 
-  // openTab() {
-    
-  // }
+  createCard() {
+    const cookie = {
+      headers:{
+        cki: this.cookieService.get("jwt")
+      } 
+    }
 
-  // name:string = "";
-  // designation:string = "";
+    console.log({data: this.postData});
 
-  // getNameValue(val:string) {
-  //   this.name = val;
-  // }
-
-  // getDesignationValue(val:string) {
-  //   this.designation = val;
-  // }
-
-  // showNameInput() {
-  //   var inputWind = document.getElementById("nameInput");
-  //   inputWind!.style.display = "block";
-  // }
-
-  // showDesignationInput() {
-  //   var inputWind = document.getElementById("designationInput");
-  //   inputWind!.style.display = "block";
-  // }
-
-  // saveNameInput() {
-  //   var inputWind = document.getElementById("nameInput");
-  //   inputWind!.style.display = "none";
-  // }
-
-  // saveDesignationInput() {
-  //   var inputWind = document.getElementById("designationInput");
-  //   inputWind!.style.display = "none";
-  // }
+    // axios.post('http://185.208.207.55/v1/api/activities/card_data/createcard', {
+    //   data: this.postData
+    // }, cookie)
+    // .then ((response) => {
+    //   console.log(response);
+    // })
+    // .catch ((error) => {
+    //   console.log(error);
+    //   alert("There was a problem. Please try again later.")
+    // })
+  }
 }
