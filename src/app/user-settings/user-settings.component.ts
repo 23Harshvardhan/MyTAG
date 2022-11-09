a0827eb49ed0e305a62a8b45530067e3fa1c1631
Harshvardhan <23harshvardhan@gmail.com>
2022-11-09 09:50:52 +0530

    Added Manual Verification
    Users can now go to settings from dashboard and verify mail manually in case they haven't.

--- src/app/user-settings/user-settings.component.ts
+++ src/app/user-settings/user-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-user-settings',
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private cookieService:CookieService
+  ) { }
+
+  setCookie(token:string){
+    this.cookieService.set('jwt', token);
+  }
+   
+  deleteCookie(cookieName:string){
+    this.cookieService.delete(cookieName);
+  }
+   
+  deleteAll(){
+    this.cookieService.deleteAll();
+  }
 
   ngOnInit(): void {
   }
 
+  otp = ""
+
+  sendOtp() {
+    const cookie = {
+      headers:{
+        cki: this.cookieService.get("jwt")
+      } 
+    }
+
+    axios.post('http://185.208.207.55/v1/api/auth/resendOtp', cookie)
+    .then ((response) => {
+      console.log(response);
+    })
+    .catch ((error) => {
+      console.log(error);
+      alert("There was a problem. Please try again later.")
+    })
+  }
+
+  verifyOtp() {
+    var otparea = document.getElementById("otpArea") as HTMLInputElement;
+    this.otp = otparea!.value;
+
+    const cookie = {
+      headers:{
+        cki: this.cookieService.get("jwt")
+      } 
+    }
+
+    axios.post('http://185.208.207.55/v1/api/auth/verifyEmail', {data: this.otp}, cookie)
+    .then ((response) => {
+      console.log(response);
+    })
+    .catch ((error) => {
+      console.log(error);
+      alert("There was a problem. Please try again later.")
+    })
+  }
 }
