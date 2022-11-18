4619d5054820ce021e89bb8b5638531eb96e5820
Harshvardhan <23harshvardhan@gmail.com>
2022-11-19 00:46:41 +0530

    Color Scheme Changed
    Color Scheme Changed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,30 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onHome(){
+    document.getElementById("home-section")?.scrollIntoView({behavior: 'smooth'});
+  }
+
+  onFeatures(){
+    document.getElementById("features-section")?.scrollIntoView({behavior: 'smooth'});
+  }
+
+  onDemo(){
+    document.getElementById("demo-section")?.scrollIntoView({behavior: 'smooth'});
+  }
+
+  onPricing(){
+    document.getElementById("pricing-section")?.scrollIntoView({behavior: 'smooth'});
+  }
+
+  onContactUs(){
+    document.getElementById("contact-section")?.scrollIntoView({behavior:'smooth'});
+  }
+
+  toggleNav() {
+    var toggle = document.getElementById("toggle");
+    var overlay = document.getElementById("overlay");
+    toggle.classList.toggle('active');
+    overlay.classList.toggle('open');
+  }
 }
