2086cc119280acaad4843820557d3b9aa10e4715
Harshvardhan <23harshvardhan@gmail.com>
2022-11-19 10:38:32 +0530

    Removed NGX Copypaste
    Removed NGX Copypaste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { QRCodeModule } from 'angularx-qrcode';
 import { NgChartsModule } from 'ng2-charts';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
-import { NgxCopyPasteModule } from 'ngx-copypaste';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -87,8 +86,7 @@ import { ViewCardComponent } from './view-card/view-card.component';
     QRCodeModule,
     NgChartsModule,
     FormsModule,
-    HttpClientModule,
-    NgxCopyPasteModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
