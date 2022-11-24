418962c3fde95d0ecccbe31a252cbd416539f1bf
Harshvardhan <23harshvardhan@gmail.com>
2022-11-24 20:20:21 +0530

    Orders Panel Working

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -12,4 +12,72 @@ export class OrdersComponent {
     toggle.classList.toggle('active');
     overlay.classList.toggle('open');
   }
+
+  data = {
+    CardID: "",
+    Date: "",
+    Country: "",
+    Type: "",
+    AccountID: "",
+    FirstName: "",
+    LastName: "",
+    UserID: "",
+    MobileNo: "",
+    ProductID: "",
+    Product: "",
+    Qty: "",
+    Amount: "",
+    Status: ""
+  }
+
+  dataSet = []
+
+  openForm() {
+
+  }
+
+  closeForm() {
+
+  }
+
+  getProductID (product:string) {
+    if(product == "NFC Plastic") {
+      return "1";
+    }
+    else if(product == "NFC Wood") {
+      return "2";
+    }
+    else {
+      return "0";
+    }
+  }
+
+  getAmount (qty:string) {
+    var amount = 120;
+    var finalAmount = amount * Number(qty);
+    return finalAmount.toString();
+  }
+
+  addOrder(creationData : {CardID:string, Country:string, Type:string, AccountID:string, FullName:string, UserID:string, MobileNo:string, Product:string, Qty:string}) {
+    var currentDate = new Date();
+    var fullName = creationData.FullName.split(' ');
+    var firstName = fullName[0];
+    var lastName = fullName[1];
+
+    this.data.CardID = creationData.CardID;
+    this.data.Date = currentDate.toDateString();
+    this.data.Country = creationData.Country;
+    this.data.Type = creationData.Type;
+    this.data.AccountID = creationData.AccountID;
+    this.data.FirstName = firstName;
+    this.data.LastName = lastName;
+    this.data.UserID = creationData.UserID;
+    this.data.MobileNo = creationData.MobileNo;
+    this.data.ProductID = this.getProductID(creationData.Product);
+    this.data.Product = creationData.Product;
+    this.data.Qty = creationData.Qty;
+    this.data.Amount = this.getAmount(creationData.Qty);
+
+    this.dataSet.push(this.data);
+  }
 }
